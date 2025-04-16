333ee6f8bcdd9ae0bc2513e66a8816b1a4eb6805
Anjina Reddy <anjinadev24@gmail.com>
2025-04-16 18:53:19 +0530

    creating ec2 instance with terraform code

---
+++ ec2/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "EC2" {
+  count =5  
+  ami           = var.ami # centos 8
+  instance_type = var.instance_type
+  #subnet_id     = aws_subnet.public_subnet.id
+  #associate_public_ip_address = true
+
+  tags = {
+    Name = "Terraform-EC2-Instance-${count.index}"
+  }
+  
+}
---
+++ ec2/variables.tf
@@ -0,0 +1,6 @@
+variable "ami" {
+  description = "The AMI ID to use for the instance"
+  type        = string
+  default     = "ami-07a6f770277670015" # Amazon Linux 2 AMI
+  
+}
