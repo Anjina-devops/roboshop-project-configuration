cda860400aa215f777d8639f88400dbb890e5a39
Anjina Reddy <anjinadev24@gmail.com>
2025-04-16 20:57:47 +0530

    changed terraform code path

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,6 +1,14 @@
 variable "ami" {
   description = "The AMI ID to use for the instance"
   type        = string
-  default     = "ami-07a6f770277670015" # Amazon Linux 2 AMI
-
+  default     = "ami-0b4f379183e5706b9" # Amazon Linux 2 AMI
+  
 }
+
+/* variable "instance_type" {
+  description = "The type of instance to create"
+  type        = string
+  default     = "t2.micro" # Change as needed
+}
+
+ */
