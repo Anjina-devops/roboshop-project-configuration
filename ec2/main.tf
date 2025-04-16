a675af7189122a81c8802574bdc38927a4214d8f
Anjina Reddy <anjinadev24@gmail.com>
2025-04-16 18:59:37 +0530

    creating ec2 instance with terraform code

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "EC2" {
-  count =5  
+  count         = 5
   ami           = var.ami # centos 8
   instance_type = var.instance_type
   #subnet_id     = aws_subnet.public_subnet.id
@@ -8,5 +8,5 @@ resource "aws_instance" "EC2" {
   tags = {
     Name = "Terraform-EC2-Instance-${count.index}"
   }
-  
+
 }
