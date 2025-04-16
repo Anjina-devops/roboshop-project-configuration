2247a76eba7432d6413420f9084fcb3c93657042
Anjina Reddy <anjinadev24@gmail.com>
2025-04-16 22:57:26 +0530

    changed terraform code path

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -7,3 +7,12 @@ output "public_ips" {
   description = "List of public IP addresses of all EC2 instances"
   value       = [for instance in aws_instance.ec2: instance.public_ip]
 }
+
+output "instance_ids" {
+  description = "List of instance IDs of all EC2 instances"
+  value       = [for instance in aws_instance.ec2: instance.id]
+}
+output "instance_tags" {
+  description = "List of tags for all EC2 instances"
+  value       = [for instance in aws_instance.ec2: instance.tags]
+}
