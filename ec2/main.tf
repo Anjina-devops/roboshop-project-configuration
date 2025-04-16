1d6eaeed501f970e2e7723dd2b24363f133b0610
Anjina Reddy <anjinadev24@gmail.com>
2025-04-16 19:09:02 +0530

    creating ec2 instance with terraform code

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "EC2" {
+resource "aws_instance" "ec2" {
   count         = 5
   ami           = var.ami # centos 8
-  instance_type = var.instance_type
+  instance_type = "t2.micro"
   #subnet_id     = aws_subnet.public_subnet.id
   #associate_public_ip_address = true
 
